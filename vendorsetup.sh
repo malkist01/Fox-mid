eb74550c00454a3c3699b7dff12018f19f64e004
Darth9 <reyprimerjefe@gmail.com>
2023-04-18 13:37:20 +0100

    Update the build vars

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2018-2022 The OrangeFox Recovery Project
+# 	Copyright (C) 2018-2023 The OrangeFox Recovery Project
 #	
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -38,8 +38,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
    	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
    	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
-   	export OF_USE_MAGISKBOOT="1"
-   	export OF_NO_MIUI_OTA_VENDOR_BACKUP="1"
    	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
 	export OF_USE_SYSTEM_FINGERPRINT="1"
 	export OF_ALLOW_DISABLE_NAVBAR=1
@@ -51,23 +49,24 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_XZ_UTILS=1
    	export FOX_REPLACE_BUSYBOX_PS=1
 	export FOX_ENABLE_APP_MANAGER=1
-
-	# use magisk 25.2 for the magisk addon
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v25.2.zip
-
-   	export OF_FORCE_DISABLE_DM_VERITY_MIUI="1"
-	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI="1"	
-
-	# OTA for custom ROMs
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-
-        # -- add settings for R11 --
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-        export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
-        # -- end R11 settings --
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800" # Tuesday,   November 14, 2017 15:20:00 GMT
+	export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+	export OF_UNBIND_SDCARD_F2FS=1
 
 	# lzma
-	export FOX_USE_LZMA_COMPRESSION=1
+	#export FOX_USE_LZMA_COMPRESSION=1
+
+	# vanilla build
+	export OF_VANILLA_BUILD=1
+
+	# no additional check for MIUI props
+	export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
+
+	# dispense with the entire OTA menu
+	export OF_DISABLE_OTA_MENU=1
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
 fi
 #
