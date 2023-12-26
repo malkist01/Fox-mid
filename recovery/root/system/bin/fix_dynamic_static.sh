1e9f720b6a98f460ecce373bce7251b6a80012dc
Darth9 <reyprimerjefe@gmail.com>
2023-12-26 08:52:34 +0000

    Deal with fstabs and other stuff for dynamic/static builds [2/2]

---
+++ recovery/root/system/bin/fix_dynamic_static.sh
@@ -0,0 +1,92 @@
+#!/system/bin/sh
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2023 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+# This script requires bash (with the binary in /system/bin/ - not /sbin/)
+
+# the device that we are building for
+BUILD_DEVICE=mido;
+
+# set to 1 while testing, else set to 0
+debug_mode=0;
+
+# write a message to the log file
+LOGMSG() {
+	echo "I:$@" >> /tmp/recovery.log;
+}
+
+# test-phase log messages
+TESTING_LOG() {
+	[ "$debug_mode" = "1" ] && LOGMSG "$@";
+}
+
+# report whether the ROM has dynamic partitions
+rom_has_dynamic_partitions() {
+  local markers=""$BUILD_DEVICE"_dynamic_partitions "$BUILD_DEVICE"_dynpart xiaomi_dynamic_partitions qti_dynamic_partitions xiaomi_dynpart qti_dynpart";
+  local F=/tmp/blck_tmp;
+  dd if=/dev/block/by-name/system bs=256k count=1 of=$F;
+  strings $F | grep dyn > "$F.txt";
+  for i in $markers
+  do
+	TESTING_LOG "Checking for $i in $F.txt";
+	if grep $i "$F.txt" > /dev/null; then
+		echo "1";
+		[ "$debug_mode" != "1" ] && rm -f $F*;
+		return;
+     	fi
+  done
+  [ "$debug_mode" != "1" ] && rm -f $F*;
+  echo "0";
+}
+
+# do the work
+process_fstab_files() {
+  local F="/system/etc/recovery.fstab";
+  local TF="/system/etc/twrp.flags";
+  local src_fstab="/system/etc/recovery-non-dynamic.fstab";
+  local src_flags="/system/etc/twrp-non-dynamic.flags";
+
+  local D=$(rom_has_dynamic_partitions);  
+  if [ "$D" = "1" ]; then
+  	src_fstab="/system/etc/recovery-dynamic.fstab";
+  	src_flags="/system/etc/twrp-dynamic.flags";
+  	TESTING_LOG "Dynamic ROM";
+  else
+    	TESTING_LOG "Non-dynamic ROM";
+  fi
+
+  # sort out the fstab files  
+  TESTING_LOG "Copying $src_fstab to $F";
+  cp -a $src_fstab $F;
+
+  TESTING_LOG "Copying $src_flags to $TF";
+  cp -a $src_flags $TF;
+
+  # cleanup
+  TESTING_LOG "Cleaning up ...";
+  rm -f /system/etc/recovery-*;
+  rm -f /system/etc/twrp-*;  
+}
+
+# --- #
+TESTING_LOG "Running $0";
+process_fstab_files;
+exit 0;
+#
