764219fb880c567de55f097cd1c8fe98603e6b9e
Darth9 <reyprimerjefe@gmail.com>
2023-12-26 08:43:06 +0000

    Remove superfluous setting

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ MITHORIUM_USES_DEVICE_SPECIFIC_FSTAB := true
 # Inherit from mithorium-common
 $(call inherit-product, device/xiaomi/mithorium-common/mithorium.mk)
 
-# Metadata
-BOARD_USES_METADATA_PARTITION := false
-
 # OEM otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/recovery/security/miui
