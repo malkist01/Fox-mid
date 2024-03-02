83a22431d721d984a9c1e77e0f0b85a729957175
Darth9 <reyprimerjefe@gmail.com>
2024-03-02 14:01:55 +0000

    Make all builds dynamic (unified)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2018-2023 The OrangeFox Recovery Project
+# 	Copyright (C) 2018-2024 The OrangeFox Recovery Project
 #	
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -34,23 +34,25 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export FOX_USE_BASH_SHELL=1
-   	export FOX_ASH_IS_BASH=1
-   	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
 	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800" # Tuesday,   November 14, 2017 15:20:00 GMT
 	export FOX_BASH_TO_SYSTEM_BIN=1
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.4.zip
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v27.0.zip
 
-       # dynamic partitions ?
-       if [ "$FOX_USE_DYNAMIC_PARTITIONS" = "1"  ]; then
-               export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-               export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-               export FOX_VARIANT="dynamic"
-       fi
+	# dynamic partitions ?
+	export FOX_USE_DYNAMIC_PARTITIONS=1; # always make dynamic builds [unified]
+
+	if [ "$FOX_USE_DYNAMIC_PARTITIONS" = "1"  ]; then
+		export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+		export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+		# export FOX_VARIANT="unified"
+	fi
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
 		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
